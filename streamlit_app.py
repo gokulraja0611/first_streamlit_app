5f48cf7e58cfb1a9e4b3d7f0a1cbae4ed390b177
Gokulraja <82395073+gokulraja0611@users.noreply.github.com>
2023-11-22 09:17:06 +0530

    Update streamlit_app.py
    added new

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit
-streamlit.title('My First Code')
-streamlit.header('Hello World')
-streamlit.text('hello world')
+streamlit.header('Breakfast Favorites')
+streamlit.text(' 🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
